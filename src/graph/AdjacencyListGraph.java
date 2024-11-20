c001c19226a7e422c123a542bdef6e1a68ab4411
Tolojanahary <79153294+AmazingTool777@users.noreply.github.com>
2024-11-20 22:10:23 +0300

    Graph / Adjacency List refactoring

--- src/graph/AdjacencyListGraph.java
+++ src/graph/AdjacencyListGraph.java
@@ -64,7 +64,8 @@ public class AdjacencyListGraph<TKey, TVertex> {
     }
 
     /**
-     * The actual adjacency list
+     * The actual adjacency list.
+     * The indices of each list are the same as the indices of the source vertices in the entries.
      */
     private final ArrayList<LinkedList<ListEdge>> lists;
 
@@ -109,6 +110,14 @@ public class AdjacencyListGraph<TKey, TVertex> {
         return this;
     }
 
+    /**
+     * The actual recursive implementation of the DFS traversal
+     *
+     * @param source         The index of the source vertex in the entries
+     * @param visited        Set that stores the indices of the visited vertices
+     * @param visitedEntries List of the entries of the visited vertices
+     * @return The visited vertex entries
+     */
     private LinkedList<VertexEntry<TKey, TVertex>> recursiveDFS(
             int source,
             TreeSet<Integer> visited,
@@ -125,12 +134,24 @@ public class AdjacencyListGraph<TKey, TVertex> {
         return visitedEntries;
     }
 
+    /**
+     * The public method that initiates the recursive DFS traversal
+     *
+     * @param source The key of the source vertex
+     * @return The list of the visited vertex entries
+     */
     public LinkedList<VertexEntry<TKey, TVertex>> recursiveDFS(TKey source) {
         return recursiveDFS(keysIndices.get(source), new TreeSet<>(), new LinkedList<>());
     }
 
+    /**
+     * Iterative implementation of the DFS traversal
+     *
+     * @param sourceKey The key of the source vertex
+     * @return The list of the visited vertex entries
+     */
     public LinkedList<VertexEntry<TKey, TVertex>> iterativeDFS(TKey sourceKey) {
-        int source = keysIndices.get(sourceKey), current;
+        int source = keysIndices.get(sourceKey), current, adj;
 
         LinkedList<VertexEntry<TKey, TVertex>> visitedVertices = new LinkedList<>();
         TreeSet<Integer> visited = new TreeSet<>();
@@ -141,25 +162,35 @@ public class AdjacencyListGraph<TKey, TVertex> {
         while (!toTraverse.isEmpty()) {
             current = toTraverse.pop();
 
+            // We skip further traversals from the current vertex if it has already been visited
             if (visited.contains(current)) continue;
 
             visitedVertices.add(entries.get(current));
             visited.add(current);
 
+            // In order to get the same order as the recursive implementation,
+            // we iterate backward over the adjacent vertices
+            // because of the stack that stores the next vertices to traverse
             LinkedList<ListEdge> adjEdges = lists.get(current);
             ListIterator<ListEdge> adjEdgesIterator = adjEdges.listIterator(adjEdges.size());
             while (adjEdgesIterator.hasPrevious()) {
-                ListEdge edge = adjEdgesIterator.previous();
-                if (visited.contains(edge.index)) continue;
-                toTraverse.push(edge.index);
+                adj = adjEdgesIterator.previous().index;
+                if (visited.contains(adj)) continue;
+                toTraverse.push(adj);
             }
         }
 
         return visitedVertices;
     }
 
+    /**
+     * BFS traversal of the graph
+     *
+     * @param sourceKey The key of the source vertex
+     * @return The list of the visited vertex entries
+     */
     public LinkedList<VertexEntry<TKey, TVertex>> BFS(TKey sourceKey) {
-        int source = keysIndices.get(sourceKey), current;
+        int source = keysIndices.get(sourceKey), current, adj;
 
         LinkedList<VertexEntry<TKey, TVertex>> visitedEntries = new LinkedList<>();
         TreeSet<Integer> visited = new TreeSet<>();
@@ -173,10 +204,11 @@ public class AdjacencyListGraph<TKey, TVertex> {
             current = toTraverse.dequeue();
 
             for (ListEdge edge : lists.get(current)) {
-                if (visited.contains(edge.index)) continue;
-                visitedEntries.add(entries.get(edge.index));
-                visited.add(edge.index);
-                toTraverse.enqueue(edge.index);
+                adj = edge.index;
+                if (visited.contains(adj)) continue;
+                visitedEntries.add(entries.get(adj));
+                visited.add(adj);
+                toTraverse.enqueue(adj);
             }
         }
 
