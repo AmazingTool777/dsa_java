357829c20e29eb635140f58864b158b6148a7d4d
Tolojanahary <79153294+AmazingTool777@users.noreply.github.com>
2024-11-22 20:32:48 +0300

    Sorting algo / Insertion sort / Docs refactoring

--- src/sortingalgorithms/InsertionSort.java
+++ src/sortingalgorithms/InsertionSort.java
@@ -9,12 +9,13 @@ public class InsertionSort<T extends Comparable<T>> implements SortAlgoStrategy<
         for (int i = 1; i < length; i++) {
             current = array[i];
 
+            // As long as the current item should precede any element amongst the sorted items,
+            // we shift the items of the sorted items until the current items find its correct location.
             int j = i;
             while (j > 0 && orderingStrategy.shouldPrecede(current, array[j - 1])) {
                 array[j] = array[j - 1];
                 j--;
             }
-
             if (j < i) {
                 array[j] = current;
             }
